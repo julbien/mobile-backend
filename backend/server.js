c40bfb4c186aeb4f8292a69f26bdfb772efeedc9
TinmPadua <christinempadua@gmail.com>
2025-07-20 21:52:37 +0800

    update server

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,7 @@ app.use(cors({
     return callback(null, true);
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Explicitly allow these methods
 }));
-
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
